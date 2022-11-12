724cb0dd7f9d5c9fbcb8b97911703b561418bb59
Serkan Koç <serkan.koc@marun.edu.tr>
2022-11-12 23:47:40 +0300

    Initial commit

---
+++ src/main/java/com/example/crmrestapi/CrmRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.crmrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crmrestapi/CrmRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crmrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
